7c2c329dc8d235cb665b15a174f653f544581096
Marlysson <marlysson5@gmail.com>
2019-12-14 23:00:41 -0300

    Setting up project.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
+  
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Logo/>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from './components/Logo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Logo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+    font-family: 'Arial', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 50px;
+  }
+
+  body{
+    background: #E74C3C;
+  }
+
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="logo">
+        <img src="../assets/logo.png" class="img-logo" />
+        <div class="line"/>
+        <span class="logo-description">Mason Shortner</span>
+    </div>
+</template>
+
+<style scoped>
+
+    .logo{
+        width:280px;
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        margin:0 auto;
+    }
+
+    .line{
+        display:inline-block;
+        width:120px;
+        height:3px;
+        background:white;
+        margin:10px auto;
+    }
+
+    .img-logo{
+        width:75px;
+        height:85px;
+    }
+
+    .logo-description{
+        font-size:30px;
+        color:white;
+    }
+</style>
